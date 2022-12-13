8b33bd1f3662775866b6fc87e6947cbc0a1ee2fc
kauzkoko <70515421+kauzkoko@users.noreply.github.com>
2022-12-13 17:13:10 +0100

    add: all inverts

--- sketchBase.js
+++ sketchBase.js
@@ -2,14 +2,21 @@
 let letters = []
 let test
 let springArray = []
+let kopf, springKopf, kBein, springKBein
 
 function setup() {
+	frameRate(60)
 	letters.push(SVG.find("#t1")[0])
 	letters.push(SVG.find("#i")[0])
 	letters.push(SVG.find("#k1")[0])
 	letters.push(SVG.find("#t2")[0])
 	letters.push(SVG.find("#a")[0])
 	letters.push(SVG.find("#k2")[0])
+	// kopf = SVG.find("#kopf")[0]
+	// springKopf = kopf.animate(new SVG.Spring(200, 15))
+	// kBein = SVG.find("#kbein")[0]
+	// springKBein = kBein.animate(new SVG.Spring(200, 15))
+	// console.log(kopf.attr())
 
 	let el = letters[0].children()[0]
 	el.animate(new SVG.Spring(400, 20))
@@ -17,28 +24,40 @@ function setup() {
 	letters.forEach((item, index) => {
 		springArray[index] = []
 		item.each(function (i, children) {
-			let me = this.animate(new SVG.Spring(400, 20))
+			let me = this.animate(new SVG.Spring(200, 15))
 			springArray[index].push(me)
 			// me.transform({ rotate: counter, origin: [this.attr("x") + this.attr("width") / 2, this.attr("y") + this.attr("height") / 2] })
 		})
 	})
 
 	//let test = letters[0].children()[0].animate(new SVG.Spring(400, 20))
-	setInterval(inti, 1000)
+	setInterval(inti, 100)
 }
 
+let intiCounter = 0
 let counter = 0
 const inti = () => {
+	console.log("counter in inti", counter)
 	letters.forEach((item, index) => {
 		item.each(function (i, children) {
 			//let me = this.animate(new SVG.Spring(400, 20))
 			springArray[index][i].transform({ rotate: counter, origin: [this.attr("x") + this.attr("width") / 2, this.attr("y") + this.attr("height") / 2] })
+			// springKopf.transform({ rotate: (intiCounter % 2) * 20 - 10, origin: [kopf.attr("cx") - kopf.attr("r") * 2, kopf.attr("cy") + kopf.attr("r")] })
+			// springKBein.transform({ rotate: (intiCounter % 2) * 20 - 10, origin: [kBein.attr("cx") - kBein.attr("r") * 2, kBein.attr("cy") + kBein.attr("r")] })
 		})
 	})
 	counter += 6
+	intiCounter++
 }
 
-function draw() {}
+function draw() {
+	if (frameCount % 60 == 0) {
+		if (floor(map(mouseX, 0, width, 0, 12)) != prevCounter) {
+			counter = floor(map(mouseX, 0, width, 0, 12))
+			prevCounter = counter
+		}
+	}
+}
 
 /*
 	createCanvas((windowHeight / 16) * 9, windowHeight)
