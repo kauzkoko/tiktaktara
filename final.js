45e9598a88741605d8b572a604b3e518547d43a9
kauzkoko <70515421+kauzkoko@users.noreply.github.com>
2022-12-21 20:50:41 +0100

    refactor: naming, remove unneeded

--- final.js
+++ final.js
@@ -1,28 +1,43 @@
+//init store for svg objects
 let letters = []
 let animatedLetters = []
-let bgh
-let circ
-let gschmeus = []
-let animatedGschmeus = []
-let shower
+let ornaments = []
+let animatedOrnaments = []
+let horizon, pendulum
+
+//pendulum config
+let pendulumSpeed = 0.8
+let pendulumCounter = 0
+let pendulumAngle = 0
+
+//object detection and interaction related config
+let lettersAreTicking = true
+let objectInsideRange = true
+let trackedX = 500
+
+//time related values
+let d, h, h12, m, s, hAngle, mAngle, sAngle
 
 function setup() {
 	noCanvas() //@paulina & luke: poster switching logics should be changed accordingly. our sketch basically takes places in a svg with id "svg" with aspect ratio of 18:16 instead of default canvas
 	setupOSC(false) // impartant! Don't modify this line.
+
 	frameRate(60)
 	angleMode(DEGREES)
-	frameRate(60)
+
+	//load svg letters and ornaments
 	letters.push(SVG.find("#t1")[0])
 	letters.push(SVG.find("#i")[0])
 	letters.push(SVG.find("#k1")[0])
 	letters.push(SVG.find("#t2")[0])
 	letters.push(SVG.find("#a")[0])
 	letters.push(SVG.find("#k2")[0])
-	gschmeus.push(SVG.find("#links-gschmeus")[0])
-	gschmeus.push(SVG.find("#rechts-gschmeus")[0])
-	bgh = SVG.find("#bgh")[0]
-	circ = SVG.find("#circ")[0]
+	ornaments.push(SVG.find("#top-ornaments")[0])
+	ornaments.push(SVG.find("#bottom-ornaments")[0])
+	horizon = SVG.find("#horizon")[0]
+	pendulum = SVG.find("#pendulum")[0]
 
+	//add ticking animations to letters and ornaments
 	letters.forEach((item, index) => {
 		animatedLetters[index] = []
 		item.each(function (i, children) {
@@ -30,54 +45,31 @@ function setup() {
 			animatedLetters[index].push(animated)
 		})
 	})
-
-	gschmeus.forEach((item, index) => {
-		animatedGschmeus[index] = []
+	ornaments.forEach((item, index) => {
+		animatedOrnaments[index] = []
 		item.each(function (i, children) {
 			let animated = this.animate(new SVG.Spring(400, 15))
-			animatedGschmeus[index].push(animated)
+			animatedOrnaments[index].push(animated)
 		})
 	})
 
-	setInterval(() => {
-		// realTime()
-		realTimeGschmeus()
-	}, 1000)
-	setInterval(dayNight, 1000 * 60)
-
-	// realTime()
-	realTimeGschmeus()
-	dayNight()
-
-	shower = document.querySelector("#shower")
+	//continuosly animate ticking elements
+	setInterval(updateOrnaments, 1000)
+	setInterval(updateHorizon, 1000 * 60)
+	updateOrnaments()
+	updateHorizon()
 }
-let enableRealTime = true
-let speed = 0.8
 
-let circCounter = 0
-let circAngle = 0
-
-let trackedX = 500
-let inside = true
 function draw() {
-	trackedX = map(posNormal.x, 0, 1.26, 0, windowWidth)
-	// console.log(windowWidth)
+	//get tracking data
+	trackedX = map(posNormal.x, 0, 1.26, 0, windowWidth) //should be normalized between 0 -1
 	trackedX = mouseX
-	console.log(trackedX)
+	if (trackedX < 50 || trackedX < windowWidth - 10) objectInsideRange = true
+	else objectInsideRange = false
 
-	circ.transform({ rotate: map(circAngle, 0, 114 / speed, -100, 100), origin: [1080, -300] })
-	circAngle += sin(frameCount * speed)
-	//circ.hide()
-
-	shower.innerHTML = windowWidth
-
-	if (trackedX < 50 || trackedX < windowWidth - 10) inside = true
-	else inside = false
-	console.log("inside", inside)
-
-	if (inside) {
-		console.log("in inside")
-		enableRealTime = false
+	// animate letters according tracker or let it tick continously
+	if (objectInsideRange) {
+		lettersAreTicking = false
 		letters.forEach((item, index) => {
 			item.each(function (i, children) {
 				let x = this.attr("x") ?? 0
@@ -97,40 +89,42 @@ function draw() {
 			})
 		})
 	} else {
-		enableRealTime = true
+		lettersAreTicking = true
 	}
 
-	console.log(enableRealTime)
+	//move the pendle
+	pendulum.transform({ rotate: map(pendulumAngle, 0, 114 / pendulumSpeed, -100, 100), origin: [1080, -300] })
+	pendulumAngle += sin(frameCount * pendulumSpeed)
 
 	posterTasks() // do not remove this last line!
 }
 
-let d, h, m, s, hAngle, mAngle, sAngle
-const realTimeGschmeus = () => {
+const updateOrnaments = () => {
 	d = new Date()
-	h = d.getHours() > 12 ? d.getHours() - 12 : d.getHours()
+	h = d.getHours()
+	h12 = d.getHours() > 12 ? d.getHours() - 12 : d.getHours()
 	m = d.getMinutes()
 	s = d.getSeconds()
-	h += m / 60
-	hAngle = h * 30
+	h12 += m / 60
+	hAngle = h12 * 30
 	mAngle = (m / 60) * 360 + 90
 	sAngle = (s / 60) * 360 + 90
 
-	gschmeus.forEach((item, index) => {
+	ornaments.forEach((item, index) => {
 		item.each(function (i, children) {
 			let x = this.attr("x") ?? 0
 			let y = this.attr("y") ?? 0
 			if (this.attr("width") == 10 || this.attr("height") == 10) {
-				animatedGschmeus[index][i].transform({ rotate: floor(sAngle), origin: [x + this.attr("width") / 2, y + this.attr("height") / 2] })
+				animatedOrnaments[index][i].transform({ rotate: floor(sAngle), origin: [x + this.attr("width") / 2, y + this.attr("height") / 2] })
 			} else if (this.attr("width") == 80 || this.attr("height") == 80) {
-				animatedGschmeus[index][i].transform({ rotate: floor(mAngle), origin: [x + this.attr("width") / 2, y + this.attr("height") / 2] })
+				animatedOrnaments[index][i].transform({ rotate: floor(mAngle), origin: [x + this.attr("width") / 2, y + this.attr("height") / 2] })
 			} else if (this.attr("width") == 160 || this.attr("height") == 160) {
 				this.transform({ rotate: floor(hAngle), origin: [x + this.attr("width") / 2, y + this.attr("height") / 2] })
 			}
 		})
 	})
 
-	if (enableRealTime) {
+	if (lettersAreTicking) {
 		letters.forEach((item, index) => {
 			item.each(function (i, children) {
 				let x = this.attr("x") ?? 0
@@ -147,9 +141,7 @@ const realTimeGschmeus = () => {
 	}
 }
 
-const realTime = () => {}
-
-const dayNight = () => {
-	if (h < 12) bgh.attr({ cy: 10000 + floor(map(h, 0, 12, 0, 1920)) })
-	else bgh.attr({ cy: 10000 + floor(map(h, 12, 24, 1920, 0)) })
+const updateHorizon = () => {
+	if (h < 12) horizon.attr({ cy: 10000 + floor(map(h, 0, 12, 0, 1920)) })
+	else horizon.attr({ cy: 10000 + floor(map(h, 12, 24, 1920, 0)) })
 }
--- utils.js
+++
@@ -1,7 +0,0 @@
-function mapperWidth(val) {
-	return map(val, 0, 1080, 0, width / 2)
-}
-
-function mapperHeight(val) {
-	return map(val, 0, 1920, 0, height)
-}
